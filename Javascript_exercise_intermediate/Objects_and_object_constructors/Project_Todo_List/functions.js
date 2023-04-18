551f4791d7dcd7cc03b7f61d10869a9a402d2eb9
Lukas <lukasdrinkwater97@gmail.com>
2023-04-18 22:10:22 +0100

    started layout of code

---
+++ Javascript_exercise_intermediate/Objects_and_object_constructors/Project_Todo_List/functions.js
@@ -0,0 +1,2 @@
+// use classes or a factory function to create each item that is added to
+// the todo list.
---
+++ Javascript_exercise_intermediate/Objects_and_object_constructors/Project_Todo_List/javascript.js
@@ -0,0 +1,7 @@
+// have a small form where you can create a new project todo list OR add a new
+// task to a specific project.
+
+// event listener that adds a new card to the DOM when a task is added to the
+// todo list
+
+// main function that runs to initialise
