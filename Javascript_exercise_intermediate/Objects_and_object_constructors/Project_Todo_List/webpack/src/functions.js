7a7a37355c3208bb177d56e26b75d917c7cd279e
Lukas <lukasdrinkwater97@gmail.com>
2023-04-20 18:34:34 +0100

    backup for debugging

--- Javascript_exercise_intermediate/Objects_and_object_constructors/Project_Todo_List/webpack/src/functions.js
+++ Javascript_exercise_intermediate/Objects_and_object_constructors/Project_Todo_List/webpack/src/functions.js
@@ -1,6 +1,6 @@
-import _ from "lodash";
-
 // class that is the project and tasks for the todo list can be added to it.
 
 // use classes or a factory function to create each item that is added to
 // the specific project todo list.
+
+export {};
