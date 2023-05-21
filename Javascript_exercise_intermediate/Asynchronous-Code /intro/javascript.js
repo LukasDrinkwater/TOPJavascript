18242746cee0d1f98bfa6cef1e33d5916d711784
Lukas <lukasdrinkwater97@gmail.com>
2023-05-21 20:30:49 +0100

    did some work on promises

---
+++ Javascript_exercise_intermediate/Asynchronous-Code /intro/javascript.js
@@ -0,0 +1,6 @@
+const myData = getData(); // if this is refactored to return a Promise...
+
+myData.then(function (data) {
+  // .then() tells it to wait until the promise is resolved
+  const pieceOfData = data["whatever"]; // and THEN run the function inside
+});
